f1ae8dcb3d3cc3ce93f4c8affce1f8b8068cdca7
Dotown <1242262161@qq.com>
2019-12-09 21:35:09 +0800

    第七天内容

--- 6-10/day7_9.py
+++ 6-10/day7_9.py
@@ -9,6 +9,7 @@ def fib(n):
 def main():
     for val in fib(20):
         print(val)
-
+# 生成器函数：常规函数定义，但是使用yield语句而不是return 语句返回结果，yield语句一次返回一个结果，可以使用多次，在每
+# 个结果中间，挂起函数的状态，以便下次从它离开的地方继续执行。
 if __name__ == '__main__':
     main()
